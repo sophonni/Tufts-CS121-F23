181e2608e2c19311a6ff178c0203dbb50085591c
Sophonni Dy <dy.sophonni@gmail.com>
2023-09-27 12:41:28 -0400

    Read contents from files

--- p2-DesignPattern/Chess.java
+++ p2-DesignPattern/Chess.java
@@ -1,22 +1,94 @@
-public class Chess {
-    public static void main(String[] args) {
-	if (args.length != 2) {
-	    System.out.println("Usage: java Chess layout moves");
-	}
-	Piece.registerPiece(new KingFactory());
-	Piece.registerPiece(new QueenFactory());
-	Piece.registerPiece(new KnightFactory());
-	Piece.registerPiece(new BishopFactory());
-	Piece.registerPiece(new RookFactory());
-	Piece.registerPiece(new PawnFactory());
-	Board.theBoard().registerListener(new Logger());
-	// args[0] is the layout file name
-	// args[1] is the moves file name
-	// Put your code to read the layout file and moves files
-	// here.
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
 
-	// Leave the following code at the end of the simulation:
-        System.out.println("Final board:");
-        Board.theBoard().iterate(new BoardPrinter());
+public class Chess {
+    public static void main(String[] args)
+    {
+        if (args.length != 2) {
+            System.out.println("Usage: java Chess layout moves");
+
+            /* SD TODO: can we throw an exception here? */
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        /* SD TODO: un-comment the bottom 7 lines before submission */
+        // Piece.registerPiece(new KingFactory());
+        // Piece.registerPiece(new QueenFactory());
+        // Piece.registerPiece(new KnightFactory());
+        // Piece.registerPiece(new BishopFactory());
+        // Piece.registerPiece(new RookFactory());
+        // Piece.registerPiece(new PawnFactory());
+        // Board.theBoard().registerListener(new Logger());
+
+        /* get file names from command line */
+        String layoutFileName = args[0]; /* args[0] is the layout file name */
+        String moveSeqFileName = args[1]; /* args[1] is the moves file name */
+        
+        File layoutFile = new File(layoutFileName);
+        File moveSeqFile = new File(moveSeqFileName);
+    
+        /* check for files existency */
+        if (layoutFile.exists() && moveSeqFile.exists())
+        {
+            /* read layout file */
+            filesParsing(layoutFile);
+            
+            /* read move sequence file */
+            filesParsing(moveSeqFile);
+        }
+        else
+        {
+            /* give layout file name doesn't exist in current directory */
+            if (!layoutFile.exists())
+            {
+                System.out.println(String.format("Error: {%1$s} file does not exist in current directory.", layoutFileName));
+            }
+            
+            /* give move sequence file name doesn't exist in current directory */
+            if (!moveSeqFile.exists())
+            {
+                System.out.println(String.format("Error: {%1$s} file does not exist in current directory.", moveSeqFileName));
+            }
+        }
+
+        // // Leave the following code at the end of the simulation:
+        /* SD TODO: un-comment the bottom 2 lines before submission */
+        // System.out.println("Final board:");
+        // Board.theBoard().iterate(new BoardPrinter());
     }
+
+    private static void filesParsing(File fileToProcess)
+    {
+        try
+        {
+            FileReader fileReader = new FileReader(fileToProcess);
+            BufferedReader textReader = new BufferedReader(fileReader);
+            String currentLineContent = "";
+
+            while ((currentLineContent = textReader.readLine()) != null)
+            {
+                if (currentLineContent.contains("#"))
+                {
+                    /* get the index of the character "#" from the current line */
+                    int commentIndex = currentLineContent.indexOf("#");
+
+                    /* check if there exist the character "#" in the current line */
+                    if (commentIndex != -1) {
+                        /* remove everything after "#" and keep only the part before it */
+                        currentLineContent = currentLineContent.substring(0, commentIndex);
+                    }
+                    
+                }
+                System.out.println(currentLineContent);
+            }
+            fileReader.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error: " + e.toString());
+        }
+    }
+
 }
--- p2-DesignPattern/Test.java
+++ p2-DesignPattern/Test.java
@@ -4,16 +4,130 @@ public class Test {
     // with assertions disabled, the default, then assert statements
     // will not execute!)
 
-    public static void test1() {
-	Board b = Board.theBoard();
-	Piece.registerPiece(new PawnFactory());
-	Piece p = Piece.createPiece("bp");
-	b.addPiece(p, "a3");
-	assert b.getPiece("a3") == p;
+    // public static void test1()
+    // {
+    //     Board b = Board.theBoard();
+    //     Piece.registerPiece(new PawnFactory());
+    //     Piece p = Piece.createPiece("bp");
+    //     b.addPiece(p, "a3");
+    //     assert b.getPiece("a3") == p;
+    // }
+
+    public static void filesParsing_NotEnoughArgument()
+    {
+        //Chess c = new Chess();
+        String[] fileNames = new String[1];
+        fileNames[0] = "move1";
+        
+        try
+        {
+            Chess.main(fileNames);
+        }
+        catch (Exception e)
+        {
+            assert e instanceof Exception == true;
+        }
+    }
+
+    public static void filesParsingTest_TooManyArguments()
+    {
+        //Chess c = new Chess();
+        String[] fileNames = new String[3];
+        fileNames[0] = "layout1";
+        fileNames[1] = "moves1";
+        fileNames[2] = "moves1";
+
+        
+        try
+        {
+            Chess.main(fileNames);
+        }
+        catch (Exception e)
+        {
+            assert e instanceof Exception == true;
+        }
+    }
+
+    public static void filesParsingTest_CorrectFilesAndFormat()
+    {
+        //Chess c = new Chess();
+        String[] fileNames = new String[2];
+        fileNames[0] = "layout1";
+        fileNames[1] = "moves1";
+
+        
+        try
+        {
+            Chess.main(fileNames);
+        }
+        catch (Exception e)
+        {
+            assert e instanceof Exception == true;
+        }
+    }
+
+    public static void filesParsingTest_NonExistanceFileName()
+    {
+        //Chess c = new Chess();
+        String[] fileNames = new String[2];
+        fileNames[0] = "layout";
+        fileNames[1] = "moves1";
+
+        
+        try
+        {
+            Chess.main(fileNames);
+        }
+        catch (Exception e)
+        {
+            assert e instanceof Exception == true;
+        }
+    }
+    public static void filesParsingTest_NonExistanceFileNames()
+    {
+        //Chess c = new Chess();
+        String[] fileNames = new String[2];
+        fileNames[0] = "layout";
+        fileNames[1] = "moves";
+
+        
+        try
+        {
+            Chess.main(fileNames);
+        }
+        catch (Exception e)
+        {
+            assert e instanceof Exception == true;
+        }
+    }
+
+    public static void filesParsingTest_CommentAtEndAndMid()
+    {
+        //Chess c = new Chess();
+        String[] fileNames = new String[2];
+        fileNames[0] = "commentAtEndAndMid.txt";
+        fileNames[1] = "moves1";
+
+        
+        try
+        {
+            Chess.main(fileNames);
+        }
+        catch (Exception e)
+        {
+            assert e instanceof Exception == true;
+        }
     }
     
-    public static void main(String[] args) {
-	test1();
+    public static void main(String[] args)
+    {
+	    //test1();
+        //filesParsing_NotEnoughArgument();
+        //filesParsingTest_TooManyArguments();
+        //filesParsingTest_CorrectFilesAndFormat();
+        //filesParsingTest_NonExistanceFileName();
+        //filesParsingTest_NonExistanceFileNames();
+        //filesParsingTest_CommentAtEndAndMid();
     }
 
 }
