1be0d940f96b34a1cff85b50d9b0db6923b55aae
Sophonni Dy <dy.sophonni@gmail.com>
2023-10-23 19:59:28 -0400

    P3: Initial Commit

---
+++ p3-UnitTestFramework/Unit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Unit {
+    public static Map<String, Throwable> testClass(String name) {
+	throw new UnsupportedOperationException();
+    }
+
+    public static Map<String, Object[]> quickCheckClass(String name) {
+	throw new UnsupportedOperationException();
+    }
+}
