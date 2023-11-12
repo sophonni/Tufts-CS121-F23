e16d7bdbb059f2a44d717879a3495a42ef9e83eb
Sophonni Dy <dy.sophonni@gmail.com>
2023-11-11 23:06:03 -0500

    P4: change from Book as model to just mmodel

--- p4-jrails/jrails/src/main/java/jrails/Model.java
+++ p4-jrails/jrails/src/main/java/jrails/Model.java
@@ -5,60 +5,60 @@ import java.lang.reflect.*;
 import java.io.*;
 
 public class Model {
-    public static Map<Integer, Map<String, Object>> Books_to_Info_Mapping = new LinkedHashMap<Integer, Map<String, Object>>();
-    public static int bookID = 0;
-    private int currBookID = 0;
+    public static Map<Integer, Map<String, Object>> AllModels = new LinkedHashMap<Integer, Map<String, Object>>();
+    public static int modelID = 0;
+    private int currModelID = 0;
     private static String dbFileName = "Book_DB.txt";
     public Model() {
         /* each model created has a unique ID */
-        currBookID = bookID;
-        Books_to_Info_Mapping.put(currBookID, null);
-        bookID++;
+        currModelID = modelID;
+        AllModels.put(currModelID, null);
+        modelID++;
     }
 
     public void save() {
         
-        /* get the class of the newly created book */
+        /* get the class of the newly created model */
         Class<?> runtimeClassOfGivenClass = this.getClass();
 
-        /* get info of the newly created book and store them */
+        /* get info of the newly created model and store them */
         Field[] publicFields = runtimeClassOfGivenClass.getFields();
-        getBookInfoAndStore(publicFields);
+        getModelInfoAndStore(publicFields);
         storeBookInDB();
-        //System.err.println("Books: " + Books_to_Info_Mapping);
+        //System.err.println("Books: " + AllModels);
     }
 
     private void storeBookInDB()
     {
-        String bookInfo = null;
+        String modelInfo = null;
         int currID = 0;
-        System.out.println("Map size: " + Books_to_Info_Mapping.size());
-        System.out.println("All Books: " + Books_to_Info_Mapping);
+        System.out.println("Map size: " + AllModels.size());
+        System.out.println("All Books: " + AllModels);
         System.err.println("ID: " +this.id());
         try
         {
             /* write all added models and their info, to the DB (i.e output file) */
             FileWriter myWriter = new FileWriter(dbFileName);
-            for (Map.Entry<Integer, Map<String, Object>> book : Books_to_Info_Mapping.entrySet())
+            for (Map.Entry<Integer, Map<String, Object>> model : AllModels.entrySet())
             {
                 /* key KVP of model's ID and all of its info */
-                int bookID = book.getKey();
-                Map<String, Object> currBookInfo = book.getValue();
-                bookInfo = "ID: " + bookID;
-                System.out.println("Current Book: " + currBookInfo);
+                int modelID = model.getKey();
+                Map<String, Object> currModelInfo = model.getValue();
+                modelInfo = "ID: " + modelID;
+                System.out.println("Current model: " + currModelInfo);
 
                 /* iterate through all of the model's info and print it to the DB (i.e output file) */
-                for (Map.Entry<String, Object> infoOfCurrBook : currBookInfo.entrySet())
+                for (Map.Entry<String, Object> infoOfCurrModel : currModelInfo.entrySet())
                 {
                     /* key KVP of model's info and its value */
-                    String infoKey = infoOfCurrBook.getKey();
-                    Object infoValue = infoOfCurrBook.getValue();
+                    String infoKey = infoOfCurrModel.getKey();
+                    Object infoValue = infoOfCurrModel.getValue();
         
-                    bookInfo += ", " + infoKey + ": " + infoValue;
+                    modelInfo += ", " + infoKey + ": " + infoValue;
                 }
                 /* each row of the DB (i.e output file) correspond to a model and its info*/
-                bookInfo += "\n";
-                myWriter.write(bookInfo);
+                modelInfo += "\n";
+                myWriter.write(modelInfo);
             }
             myWriter.close();
         }
@@ -69,9 +69,9 @@ public class Model {
         }
     }
 
-    private void getBookInfoAndStore(Field[] publicFields)
+    private void getModelInfoAndStore(Field[] publicFields)
     {
-        Map<String, Object> bookInfo_to_valueOfInfo_mapping = new LinkedHashMap<String, Object>();
+        Map<String, Object> infoOfAModel = new LinkedHashMap<String, Object>();
         String title = null;
         String author = null;
         Object num_copies = 0;
@@ -90,57 +90,57 @@ public class Model {
                         {
                             case "title":
                                 title = field.get(this).toString();
-                                // /* ensure that the book has title */
+                                // /* ensure that the model has title */
                                 // if (title == null || title.isEmpty())
                                 // {
-                                //     throw new IllegalArgumentException("Error in {getBookInfoAndStore} function: {title} can't be null or empty.");
+                                //     throw new IllegalArgumentException("Error in {getModelInfoAndStore} function: {title} can't be null or empty.");
                                 // }
                                 // else
                                 // {
-                                bookInfo_to_valueOfInfo_mapping.put(field.getName(), title);
+                                infoOfAModel.put(field.getName(), title);
                                 // }
                                 break;
                             case "author":
                                 author = field.get(this).toString();
-                                // /* ensure that the book has author name */
+                                // /* ensure that the model has author name */
                                 // if (author == null || author.isEmpty())
                                 // {
-                                //     throw new IllegalArgumentException("Error in {getBookInfoAndStore} function: {author} can't be null or empty.");
+                                //     throw new IllegalArgumentException("Error in {getModelInfoAndStore} function: {author} can't be null or empty.");
                                 // }
                                 // else
                                 // {
-                                bookInfo_to_valueOfInfo_mapping.put(field.getName(), author);
+                                infoOfAModel.put(field.getName(), author);
                                 // }
                                 break;
                             case "num_copies":
                                 num_copies = field.get(this);
-                                /* ensure that the book doesn't have negative number of copies */
+                                /* ensure that the model doesn't have negative number of copies */
                                 if ((int)num_copies < 0)
                                 {
-                                    throw new IllegalArgumentException("Error in {getBookInfoAndStore} function: {num_copies} can't be negative.");
+                                    throw new IllegalArgumentException("Error in {getModelInfoAndStore} function: {num_copies} can't be negative.");
                                 }
                                 else
                                 {
-                                    bookInfo_to_valueOfInfo_mapping.put(field.getName(), num_copies);
+                                    infoOfAModel.put(field.getName(), num_copies);
                                 }
                                 break;
                             case "bookID":
-                                //bookInfo_to_valueOfInfo_mapping.put(field.getName(), bookID);
+                                //infoOfAModel.put(field.getName(), modelID);
                                 break;
                             default:
                                 otherField = field.get(this);
-                                bookInfo_to_valueOfInfo_mapping.put(field.getName(), otherField);
+                                infoOfAModel.put(field.getName(), otherField);
                                 break;
                         }
                     }
                     catch (Exception e)
                     {
-                        System.err.println("Error in {getBookInfoAndStore} function: " + e);
+                        System.err.println("Error in {getModelInfoAndStore} function: " + e);
                     }
                 }
                 else
                 {
-                    throw new IllegalArgumentException("Error in {getBookInfoAndStore} function: Field with @Column annotation should be of type Boolean, String, or Integer.");
+                    throw new IllegalArgumentException("Error in {getModelInfoAndStore} function: Field with @Column annotation should be of type Boolean, String, or Integer.");
                 }
 
             }
@@ -151,18 +151,18 @@ public class Model {
         // System.err.println("num_copies: " + num_copies);
 
         /* ensure that the ID of a model exist before modifying the model's info */
-        if (!Books_to_Info_Mapping.containsKey(this.currBookID))
+        if (!AllModels.containsKey(this.currModelID))
         {
-            throw new IllegalArgumentException("Error in {getBookInfoAndStore} function: book with ID {" + this.currBookID + "} does not exist in the DB.");
+            throw new IllegalArgumentException("Error in {getModelInfoAndStore} function: model with ID {" + this.currModelID + "} does not exist in the DB.");
         }
         else
         {
-            Books_to_Info_Mapping.put(this.currBookID, bookInfo_to_valueOfInfo_mapping);
+            AllModels.put(this.currModelID, infoOfAModel);
         }
     }
 
     public int id() {
-        return this.currBookID;
+        return this.currModelID;
     }
 
     public static <T> T find(Class<T> c, int id) {
@@ -190,6 +190,6 @@ public class Model {
             System.err.println("An error occurred.");
             e.printStackTrace();
         }
-        Books_to_Info_Mapping.clear();
+        AllModels.clear();
     }
 }
